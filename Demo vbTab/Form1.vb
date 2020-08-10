066d69fa6662fd9a4ab0086e77df57366c6f0a9c
RizkyKhapidsyah <RizkyKhapidsyah@RIKY_PC>
2020-08-10 20:47:50 +0700

    Add project files.

---
+++ Demo vbTab/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        kotakList.Items.Add("012345678901234567890")
+        kotakList.Items.Add("X" & vbTab & "X")
+        kotakList.Items.Add("XXXXXXXXXXXX" & vbTab & "X")
+        kotakList.Items.Add(vbTab & vbTab & "X")
+    End Sub
+End Class
